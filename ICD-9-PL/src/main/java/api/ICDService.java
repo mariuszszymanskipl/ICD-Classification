8a57e247ee301b982761c3afbd6c7a97104dacd5
Mariusz Szymanski <mariusz.szymanski@sayen.pl>
2017-07-02 13:12:16 +0200

    rest method get returns correct all classification

--- ICD-9-PL/src/main/java/api/ICDService.java
+++ ICD-9-PL/src/main/java/api/ICDService.java
@@ -1,18 +1,14 @@
 package api;
 
 import domain.Classification;
-import domain.Section;
 import services.ICDBuilder;
 
 import javax.inject.Inject;
-import javax.json.Json;
-import javax.json.JsonArrayBuilder;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.List;
 
 /**
  * @author Mariusz Szymanski
@@ -29,12 +25,7 @@ public class ICDService {
     public Response getAllClassification() {
 
         Classification classification = icdBuilder.getAllClassification();
-        List<Section> sections = classification.getSections();
-        JsonArrayBuilder jsonArrayBuilder = Json.createArrayBuilder();
-        for (Section section : sections) {
-            jsonArrayBuilder.add(section.toJsonObject());
-        }
-//        return Response.status(Response.Status.OK).entity(classification).build();
-        return Response.status(Response.Status.OK).entity(jsonArrayBuilder.build()).build();
+
+        return Response.status(Response.Status.OK).entity(classification).build();
     }
 }
